cf8cf39b21f5e0f7b3d360aadfccc615ac3ac2eb
salman <phpafridi@gmail.com>
2026-04-07 10:16:37 +0500

    final test 16.2

---
+++ database/migrations/2026_04_06_101258_add_timestamps_to_p_parts_table.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up()
+    {
+        Schema::table('p_parts', function (Blueprint $table) {
+            $table->timestamps(); // This adds created_at and updated_at
+        });
+    }
+
+    public function down()
+    {
+        Schema::table('p_parts', function (Blueprint $table) {
+            $table->dropTimestamps();
+        });
+    }
+};
